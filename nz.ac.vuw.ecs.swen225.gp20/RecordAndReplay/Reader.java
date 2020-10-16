66887f38fc787f29092969ae87245c367a8260bb
choijere <choijere@myvuw.ac.nz>
2020-10-16 15:06:23 +1300

    Wrote myself some plans.

--- nz.ac.vuw.ecs.swen225.gp20/RecordAndReplay/Reader.java
+++ nz.ac.vuw.ecs.swen225.gp20/RecordAndReplay/Reader.java
@@ -1,10 +1,38 @@
 package RecordAndReplay;
 
+import RecordAndReplay.Actions.Action;
+
+import java.util.ArrayList;
+
 /**
  * Should be able to read a json file.
  * Upon bad formatting, closes with a failure state.
  * When successful, should be able to report an arraylist of arraylists of actions.
+ *
+ * Reminder to self:
+ * So Essentially, the player should select when they wanna start a replay.
+ * Warn the player about how their current game session will end.
+ * They proceed, All the created saves should be presented in the next alert box. (gives an error if there is none and returns to their game)
+ * They select a save, The json save should be loaded, via this reader, into the recorded changes. (gives an error on bad formatting and returns to their game)
+ * Immediately resets the game, and loads the player in their beginning spot OF said recording (NOTE: you'll also need to deploy creatures
+ * in their beginning spots too!)
+ * All this class does is return "recordedChanges" however.
+ *
+ * In "Replayer" (recordedChanges should be loaded in RecordAndReplay) the player may use arrow keys to go left or right in the recording.
+ * If there is no more recording, an alert should display "END OF RECORDING".
+ * If the player rewinds too far, an alert shouldn't display but the Replayer shouldn't allow it to rewind.
  */
 public class Reader {
+    ArrayList<ArrayList<Action>> recordedChanges = new ArrayList<ArrayList<Action>>();
+    public Reader() {
+        //empty constructor
+    }
 
+    //goes through all the objects in "saves" and
+    //ANOTHER REMINDER TO SELF: players should be able to name their saves
+    public ArrayList<String> findSaves() {
+        return null;
+    }
+
+    //read json
 }
